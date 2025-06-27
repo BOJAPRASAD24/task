19af87ce2a442403df66e60ae0f3af70ea02bdd4
BOJAPRASAD24 <bojaprasad1998@gmail.com>
2025-06-27 12:42:27 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl front-black text-center">
+      Hello world
+        </h1>
+
+    <div className="flex flex-wrap gap-5">
+      <div className="w-80 h-80 bg-green-400">
+      </div>
+      <div className="w-80 h-80 bg-cyan-400">
+      </div>
+
+    </div>
+
+    </div>
+  );
+}
