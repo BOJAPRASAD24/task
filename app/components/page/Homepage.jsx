e3611a2c10833dc9488e20fa3c37434229b156d9
BOJAPRASAD24 <bojaprasad1998@gmail.com>
2025-07-01 13:33:47 +0530

    Initial commit

--- app/components/page/Homepage.jsx
+++ app/components/page/Homepage.jsx
@@ -1,25 +1,58 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../common/Header';
-import Link from 'next/link';
 
 const Homepage = () => {
   const name = "Bob";
 
-  const handleClick = () =>{
-    alert("Hello World!");
-  }
+  // let count = 0;
+  const [count,setCount] = useState(0);
+  const [count1,setCount1] = useState(10);
+
+const handleIncreament = ()=>{
+  setCount(count + 1);
+
+}
+
+const handleIncreament1 = ()=>{
+  setCount(count1 + 10);
+
+}
+
+
+// useEffect(() => {
+//   console.log("Render!")
+// })
+
+// useEffect(()=>{
+//   console.log("Render!");
+// },[]);
+
+// useEffect(()=>{
+//   console.log("Render!");
+// },[count1]);
 
     return (
       <div className='m-4 '>
         <Header username={name}/>
-<Link href={'/about'}>About</Link>
-        <div>
-           <h1>Hi,{name}</h1>
-        </div>
+        <div className='m-8'>
 
-        <button onClick={handleClick}className='bg-blue-600 text-white px-4 py-2 rounded cursor-pointer' >Click /</button>
+          <h1 className='text-4xl font-semibold '>
+            {count}
+          </h1>
+
+        <button onClick={handleIncreament}className='bg-green-500 text-white py-2 px-4 rounded'>+</button>
+       </div>
+
+      <div className='m-8'>
+
+        <h1 className='text-4xl font-semibold '>
+         {count1}
+       </h1>
+
+       <button onClick={handleIncreament1}className='bg-green-500 text-white py-2 px-4 rounded'>+</button>
       </div>
+    </div>
     )
   }
 
